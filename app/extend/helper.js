2c5d11afae6ec8f0937bcb025ea1bf151aadeef7
Marhooo <105950987@qq.com>
2021-06-29 23:49:18 +0800

    add crypto-js and js-base64 && fix login && add vertify encryptInfo

--- app/extend/helper.js
+++ app/extend/helper.js
@@ -1,10 +1,12 @@
-const crypto = require("crypto");
-const path = require("path");
-const fs = require("fs");
-const dayjs = require("dayjs");
-const sendToWormhole = require("stream-wormhole")
-const awaitStreamReady = require("await-stream-ready").write
-const util = require("util")
+const crypto = require('crypto');
+const path = require('path');
+const fs = require('fs');
+const dayjs = require('dayjs');
+const sendToWormhole = require('stream-wormhole');
+const awaitStreamReady = require('await-stream-ready').write;
+const util = require('util');
+const CryptoJS = require('crypto-js');
+const { Base64 } = require('js-base64');
 
 module.exports = {
   //微信APIv3密钥解密微信平台证书加密公钥函数，解密出来的公钥放入'app/ca'
@@ -12,8 +14,11 @@ module.exports = {
 
   //微信支付签名函数
   wechatPaySignCrypto(message) {
-    const privateKeyCertificate = fs.readFileSync('app/ca/apiclient_key.pem')
-    return crypto.createSign('sha256WithRSAEncryption').update(message).sign(privateKeyCertificate, "base64")    
+    const privateKeyCertificate = fs.readFileSync('app/ca/apiclient_key.pem');
+    return crypto
+      .createSign('sha256WithRSAEncryption')
+      .update(message)
+      .sign(privateKeyCertificate, 'base64');
   },
 
   //微信支付签名验证函数
@@ -36,13 +41,28 @@ module.exports = {
   //     oaepHash: 'sha1',
   //     key: privateKeyCertificate,
   //     padding: crypto.constants.RSA_PKCS1_OAEP_PADDING,
-  //   }, Buffer.from(ciphertext, 'base64')).toString('utf8');    
+  //   }, Buffer.from(ciphertext, 'base64')).toString('utf8');
   // },
+
+  //微信用户加密信息解密
+  wxUserDecryptData(sessionKey, data, ivv) {
+    let key = CryptoJS.enc.Base64.parse(sessionKey);
+    let iv = CryptoJS.enc.Base64.parse(ivv);
+    let decrypt = CryptoJS.AES.decrypt(data, key, {
+      iv: iv,
+      mode: CryptoJS.mode.CBC,
+      padding: CryptoJS.pad.Pkcs7,
+    });
+    return JSON.parse(Base64.decode(CryptoJS.enc.Base64.stringify(decrypt)));
+  },
 
   // MD5 对密码和秘钥进行混合双重加密
   async cryptoMd5(password, key) {
-    const hash1 = await crypto.createHash("md5").update(password).digest("hex");
-    const hash2 = await crypto.createHash("md5").update(hash1 + key).digest("hex");
+    const hash1 = await crypto.createHash('md5').update(password).digest('hex');
+    const hash2 = await crypto
+      .createHash('md5')
+      .update(hash1 + key)
+      .digest('hex');
     return hash2;
   },
 
@@ -51,8 +71,8 @@ module.exports = {
     this.ctx.body = {
       code: code,
       data: {
-        message
-      }
+        message,
+      },
     };
     this.ctx.status = status;
   },
@@ -60,37 +80,37 @@ module.exports = {
   // 创建 Token
   createToken(data, expires, strTimer) {
     return this.app.jwt.sign(data, this.app.config.jwt.secret, {
-      expiresIn: expires + " " + strTimer,
+      expiresIn: expires + ' ' + strTimer,
     });
   },
 
   // 获取 Token
-  getAccessToken () {
-    const bearerToken = this.ctx.request.header.authorization
-    return bearerToken && bearerToken.replace("Bearer ", "")
+  getAccessToken() {
+    const bearerToken = this.ctx.request.header.authorization;
+    return bearerToken && bearerToken.replace('Bearer ', '');
   },
 
   // 校验 Token
-  async verifyToken () {
-    const that = this
-    let backResult = false
-    const token = this.getAccessToken()
+  async verifyToken() {
+    const that = this;
+    let backResult = false;
+    const token = this.getAccessToken();
     const verify = await function (token) {
-      const result = {}
+      const result = {};
       that.app.jwt.verify(token, that.app.config.jwt.secret, function (err, decoded) {
         if (err) {
-          result.verify = false
-          result.message = err.message
+          result.verify = false;
+          result.message = err.message;
         } else {
-          result.verify = true
-          result.message = decoded
+          result.verify = true;
+          result.message = decoded;
         }
-      })
+      });
       // console.log(result);
-      return result
-    }
-    const verifyResult = verify(token)
-    const tokenInfo = await this.ctx.service.admin.login.findToken(token)
+      return result;
+    };
+    const verifyResult = verify(token);
+    const tokenInfo = await this.ctx.service.admin.login.findToken(token);
 
     if (!verifyResult.verify) {
       // 2小时的 token 验证失败了
@@ -98,63 +118,61 @@ module.exports = {
         // 能查到对应的 refresh_token
         if (!verify(tokenInfo.refresh_token).verify) {
           // 2小时的 token 验证失败了并且7天的 token 验证也失败了
-          await this.error(401, 10000, "token身份认证失效,请重新登录")
+          await this.error(401, 10000, 'token身份认证失效,请重新登录');
         } else {
           // 2小时的 token 验证失败了,但是能查到对应的 refresh_token 并且在有效期内就重新生成新的 token
-          const refresh_token = await this.createToken({ id: tokenInfo.uid }, "7", "days")
-          const access_token = await this.createToken({ id: tokenInfo.uid }, "2", "hours")
-          const { id, uid } = { id: tokenInfo.id, uid: tokenInfo.uid }
-          await this.ctx.service.admin.login.saveToken({ id, uid, access_token, refresh_token })
-          await this.error(200, 11000, access_token)
-
+          const refresh_token = await this.createToken({ id: tokenInfo.uid }, '7', 'days');
+          const access_token = await this.createToken({ id: tokenInfo.uid }, '2', 'hours');
+          const { id, uid } = { id: tokenInfo.id, uid: tokenInfo.uid };
+          await this.ctx.service.admin.login.saveToken({ id, uid, access_token, refresh_token });
+          await this.error(200, 11000, access_token);
         }
       } else {
         // 2小时的 token 验证失败了并且查不到对应的 refresh_token
-        await this.error(401, 10000, "token身份认证失效,请重新登录")
+        await this.error(401, 10000, 'token身份认证失效,请重新登录');
       }
-
     } else {
       if (tokenInfo) {
         // 2小时的 token 验证通过了并且可以查到对应的 refresh_token
-        backResult = true
+        backResult = true;
       } else {
         // 2小时的 token 验证通过了但是查不到对应的 refresh_token
-        this.error(401, 10000, "该账号已在其他地方登陆,请重新登录")
+        this.error(401, 10000, '该账号已在其他地方登陆,请重新登录');
       }
     }
-    return backResult
+    return backResult;
   },
 
-
   // 上传图片
-  async uploadImg () {
-    let imgUrl
+  async uploadImg() {
+    let imgUrl;
     // 获取 steam
-    const stream = await this.ctx.getFileStream()
+    const stream = await this.ctx.getFileStream();
     // 上传基础目录
-    const uploadBasePath = "app/public/upload/"
+    const uploadBasePath = 'app/public/upload/';
     // 生成文件名
-    const filename = Date.now() + "" + Number.parseInt(Math.random() * 10000) + path.extname(stream.filename)
+    const filename =
+      Date.now() + '' + Number.parseInt(Math.random() * 10000) + path.extname(stream.filename);
     // 生成文件夹
-    const dirName = dayjs(Date.now()).format("YYYYMMDD")
-    if (!fs.existsSync(path.join(this.config.baseDir, uploadBasePath, dirName))){
-      fs.mkdirSync(path.join(this.config.baseDir, uploadBasePath, dirName))
+    const dirName = dayjs(Date.now()).format('YYYYMMDD');
+    if (!fs.existsSync(path.join(this.config.baseDir, uploadBasePath, dirName))) {
+      fs.mkdirSync(path.join(this.config.baseDir, uploadBasePath, dirName));
     }
     // 生成写入路径
-    const target = path.join(this.config.baseDir, uploadBasePath, dirName, filename)
+    const target = path.join(this.config.baseDir, uploadBasePath, dirName, filename);
     // 写入流
-    const writeStream = fs.createWriteStream(target)
+    const writeStream = fs.createWriteStream(target);
     try {
       // 写入文件
-      await awaitStreamReady(stream.pipe(writeStream))
-      imgUrl = "http://" + this.ctx.request.header.host + "/public/upload/" + dirName + "/" + filename
-
+      await awaitStreamReady(stream.pipe(writeStream));
+      imgUrl =
+        'http://' + this.ctx.request.header.host + '/public/upload/' + dirName + '/' + filename;
     } catch (err) {
       // 必须将上传的文件流消费掉，要不然浏览器响应会卡死
-      await sendToWormhole(stream)
-      throw err
+      await sendToWormhole(stream);
+      throw err;
     }
-    return imgUrl
+    return imgUrl;
   },
 
   //下载文件
@@ -165,6 +183,5 @@ module.exports = {
     this.ctx.set('Content-Type', 'application/octet-stream');
     this.ctx.set('Content-Length', fileSize);
     this.ctx.body = fs.createReadStream(filePath);
-  }
-
+  },
 };
--- app/service/customer/wxlogin.js
+++ app/service/customer/wxlogin.js
@@ -1,10 +1,11 @@
 const Service = require('egg/index').Service;
 
 class WxLoginService extends Service {
-  //微信拿到openid后放入数据库，并换取token时考虑账号是否已经存在
+  //微信小程序发来请求有两种情况：1.重新授权用户信息和手机信息后发来的请求。 2.一般微信小程序请求后台登录拿token
   async wxRegisterLogin(options) {
+    const transaction = await this.ctx.model.transaction();
     try {
-      const { js_code, userInfo } = options;
+      const { js_code } = options;
       const url =
         'https://api.weixin.qq.com/sns/jscode2session?appid=' +
         this.app.config.wechat.appid +
@@ -16,64 +17,101 @@ class WxLoginService extends Service {
       const resOidAndSkey = await this.ctx.curl(url, {
         dataType: 'json',
       });
-      //console.log(resOidAndSkey)
       if (resOidAndSkey.data && resOidAndSkey.status === 200) {
-        const roleinfo = await this.ctx.model.SystemRole.findOne({
-          where: { name: '游客' },
-        });
-        const user = await this.ctx.service.customer.wxlogin.findOpenId(resOidAndSkey.data.openid);
-        if (!user) {
-          await this.ctx.model.SystemUser.create({
-            username: 'wxlogin',
-            password: 'wxlogin',
-            openid: resOidAndSkey.data.openid,
-            role_id: roleinfo.id,
-            session_key: resOidAndSkey.data.session_key,
-            name: userInfo.nickName,
-            info: `${userInfo.province},${userInfo.city}`,
-            avatar: userInfo.avatarUrl,
-            sex: userInfo.gender == 1 ? 1 : 2,
+        if(options.userEncryptedData && options.phoneEncryptedData && options.userIv && options.phoneIv) {
+          //微信小程序重新授权用户信息和手机信息后发来的请求
+          let userInfo = this.ctx.helper.wxUserDecryptData(resOidAndSkey.data.session_key, options.userEncryptedData, options.userIv)
+          let userPhoneInfo = this.ctx.helper.wxUserDecryptData(resOidAndSkey.data.session_key, options.phoneEncryptedData, options.phoneIv)
+          // console.log(userPhoneInfo)
+          // console.log(userInfo)
+          const roleInfo = await this.ctx.model.SystemRole.findOne({
+            where: { name: '游客' },
           });
-          const checkUser = await this.ctx.service.customer.wxlogin.findOpenId(
-            resOidAndSkey.data.openid
-          );
-          if (checkUser) {
-            const refresh_token = await this.ctx.helper.createToken(
-              { id: checkUser.id },
-              '7',
-              'days'
-            );
-            const access_token = await this.ctx.helper.createToken(
-              { id: checkUser.id },
-              '2',
-              'hours'
-            );
-            const uid = checkUser.id;
-            await this.ctx.model.SystemToken.upsert({
-              uid,
-              access_token,
-              refresh_token,
-            });
-            this.ctx.body = {
-              code: 200,
-              data: { access_token },
-            };
-          } else {
-            this.ctx.helper.error(200, 10204, 'token获取失败');
-          }
-        } else {
-          await this.ctx.model.SystemUser.update(
-            {
+          const user = await this.ctx.service.customer.wxlogin.findOpenId(resOidAndSkey.data.openid);
+          if (!user) {
+            await this.ctx.model.SystemUser.create({
+              username: 'wxlogin',
+              password: 'wxlogin',
+              openid: resOidAndSkey.data.openid,
+              unionid: resOidAndSkey.data.unionid,
+              role_id: roleInfo.id,
+              session_key: resOidAndSkey.data.session_key,
               name: userInfo.nickName,
               info: `${userInfo.province},${userInfo.city}`,
               avatar: userInfo.avatarUrl,
-              sex: userInfo.gender == 1 ? 1 : 2,
-            },
-            {
-              where: {
-                id: user.id,
-              },
+              sex: userInfo.gender == 1 ? '1' : '2',
+              mobile_phone: userPhoneInfo.phoneNumber
+            }, {transaction});
+            const checkUser = await this.ctx.service.customer.wxlogin.findOpenId(
+              resOidAndSkey.data.openid
+            );
+            if (checkUser) {
+              const refresh_token = await this.ctx.helper.createToken(
+                { id: checkUser.id },
+                '7',
+                'days'
+              );
+              const access_token = await this.ctx.helper.createToken(
+                { id: checkUser.id },
+                '2',
+                'hours'
+              );
+              const uid = checkUser.id;
+              await this.ctx.model.SystemToken.upsert({
+                uid,
+                access_token,
+                refresh_token,
+              }, {transaction});
+              await transaction.commit();
+              this.ctx.body = {
+                code: 200,
+                data: { access_token },
+              };
+            } else {
+              await transaction.rollback();
+              this.ctx.helper.error(200, 10204, 'token获取失败');
             }
+          } else {
+            await this.ctx.model.SystemUser.update(
+              {
+                name: userInfo.nickName,
+                info: `${userInfo.province},${userInfo.city}`,
+                avatar: userInfo.avatarUrl,
+                sex: userInfo.gender == 1 ? '1' : '2',
+                mobile_phone: userPhoneInfo.phoneNumber
+              },
+              {
+                where: {
+                  id: user.id,
+                },
+                transaction
+              }
+            );
+            const role = await this.ctx.model.SystemRole.findByPk(user.role_id);
+            if (!role.status) {
+              this.ctx.helper.error(200, 10020, '该账号所在角色已被禁用,请联系管理员');
+            } else if (!user.status || user.status == '0') {
+              this.ctx.helper.error(200, 10020, '该账号已被禁用,请联系管理员');
+            } else {
+              const refresh_token = await this.ctx.helper.createToken({ id: user.id }, '7', 'days');
+              const access_token = await this.ctx.helper.createToken({ id: user.id }, '2', 'hours');
+              const uid = user.id;
+              await this.ctx.model.SystemToken.upsert({
+                uid,
+                access_token,
+                refresh_token,
+              }, {transaction});
+              await transaction.commit();
+              this.ctx.body = {
+                code: 200,
+                data: { access_token },
+              };
+            }
+          }
+        } else {
+          //微信小程序的平常登录情况
+          const user = await this.ctx.service.customer.wxlogin.findOpenId(
+            resOidAndSkey.data.openid
           );
           const role = await this.ctx.model.SystemRole.findByPk(user.role_id);
           if (!role.status) {
@@ -88,18 +126,21 @@ class WxLoginService extends Service {
               uid,
               access_token,
               refresh_token,
-            });
+            }, {transaction});
+            await transaction.commit();
             this.ctx.body = {
               code: 200,
               data: { access_token },
             };
-          }
+          }         
         }
       } else {
+        await transaction.rollback();
         this.ctx.helper.error(200, 10500, '微信侧微服务获取失败');
       }
     } catch (err) {
       console.log(err);
+      await transaction.rollback();
       this.ctx.helper.error(200, 10404, 'token获取失败');
     }
   }
